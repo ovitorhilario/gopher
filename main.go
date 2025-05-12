1262ca03910eb5a53ff633f77e6c2a5f12b058a4
Vitor Hilário <vitorbarbosahilario@gmail.com>
2025-05-11 21:32:18 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"github.com/ovitorhilario/gopher/config"
 	"github.com/ovitorhilario/gopher/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v",err)
+		return
+	}
+
 	router.Initialize()
 }
