d5234c31a041c519f3cbcacdb35718ba6de265e9
Vitor Hilário <vitorbarbosahilario@gmail.com>
2025-05-12 00:47:48 -0300

    feat: add delete, list, show and update handlers

--- handler/request.go
+++ handler/request.go
@@ -16,6 +16,9 @@ type CreateOpeningRequest struct {
 }
 
 func (r *CreateOpeningRequest) Validate() error {
+	if r.Role == "" && r.Company == "" && r.Location == "" && r.Remote == nil && r.Salary <= 0 {
+		return fmt.Errorf("request body is empty or malformed")
+	}
 	if r.Role == "" {
 		return errParamIsRequired("role", "string")
 	}
@@ -35,4 +38,22 @@ func (r *CreateOpeningRequest) Validate() error {
 		return errParamIsRequired("salary", "int64")
 	}
 	return nil
+}
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// If any field is provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// If none of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
 }
